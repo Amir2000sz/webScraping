d0371bdcbe3493805baf4018da6328fa1f00b46e
Amirreza SalimZadeh <amir314.salim@gmailcom>
2024-04-02 20:29:44 +0330

    now we can also choose date from mr bilit

--- mrblitit.py
+++ mrblitit.py
@@ -7,9 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os 
-
+import time
 class mrbilit(webdriver.Chrome):
-    def __init__(self,checker,origin=None,destinition=None):
+    def __init__(self,checker,origin=None,destinition=None,date=None):
+        self.date = date
         self.origin = origin
         self.destination = destinition
         self.chek = checker
@@ -54,16 +55,34 @@ class mrbilit(webdriver.Chrome):
         origin.send_keys(self.origin)
         wait = WebDriverWait(self, 10)
         suggestion_list = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]")))
-        
+        time.sleep(1)
         firstOption = self.find_element(by=By.XPATH, value="/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]")
         firstOption.click()
 
     def choose_destination(self):
         desti = self.find_element(By.XPATH,"/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[2]/div/div/div[1]/div/label/input")
         self.implicitly_wait(10)
+        desti.click()
         desti.send_keys(self.destination)
         waitD = WebDriverWait(self, 10)
         suggestion_list2 = waitD.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[2]/div/div/div[2]/div/div/div[2]")))
-
+        time.sleep(1)
         firstOptionD = self.find_element(by=By.XPATH, value="/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[2]/div/div/div[2]/div/div/div[2]")
         firstOptionD.click()
+    
+    def choose_date(self):
+        waitDate = WebDriverWait(self, 10)
+        suggestion_list_Date = waitDate.until(EC.visibility_of_element_located((By.CLASS_NAME, "mr-datepicker-container")))
+        while True:
+            buttonOfDAte = self.find_element(By.XPATH , "/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div[1]/div[1]/div")
+            buttonOfDAte.click()
+            self.implicitly_wait(5)
+            try:
+                
+                date = self.find_elements(By.CLASS_NAME , "day-container")
+                date[self.date-1].click()
+                break
+            except :
+                print("the date you have choosen is not available")
+                break
+    
--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from mrblitit import mrbilit
 import time
-with mrbilit(False,"jivhk","advhc") as bot:
+with mrbilit(False,"jivhk","ilnhk",20) as bot:
     bot.choose_type("bus")
     # bot.implicitly_wait(10)
-    time.sleep(1)
+    time.sleep(5)
+    bot.implicitly_wait(10)
     bot.choose_origin()
+    time.sleep(2)
+    bot.implicitly_wait(10)
     bot.choose_destination()
+    bot.implicitly_wait(10)
+    bot.choose_date()
